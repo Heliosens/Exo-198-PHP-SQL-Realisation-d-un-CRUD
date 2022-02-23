5ef4a85bcccbbc407ed60118d57881f916a90af7
Sylvie-B <unsolo@hotmail.fr>
2022-02-23 14:54:32 +0100

    update delete

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php
 require 'connPDO.php';
 $pdo = new connPDO();
@@ -19,16 +20,49 @@ function createEleve ($db, $nom, $prenom, $age){
 
 // read
 function readClasse ($db){
+    $arrayClasse = [];
     $stm = $db->prepare("SELECT * FROM eleves");
     if($stm->execute()){
         foreach ($stm->fetchAll() as $eleve){
-            echo $eleve['nom'] . "<br>";
+            $arrayClasse[$eleve['id']] = ["nom" => $eleve['nom'], "prenom" => $eleve['prenom'], "age" => $eleve['age']];
         }
     }
+    return $arrayClasse;
 }
 
+// update
+function updateEleve ($db, $id, $nom, $prenom, $age){
+    $stm = $db->prepare("
+    UPDATE eleves SET nom = :nom, prenom = :prenom, age = :age WHERE id = :id
+    ");
+    $stm->bindParam(':nom', $nom);
+    $stm->bindParam(':prenom', $prenom);
+    $stm->bindParam(':age', $age);
+    $stm->bindParam(':id', $id);
+    $stm->execute();
+}
+
+// delete
+function delEleve ($db, $id){
+    try {
+        $sql = "DELETE FROM eleves WHERE id = '$id'";
+
+        if($db->exec($sql) !== false){
+            echo "élève " . $id . " supprimé";
+        }
+    }
+    catch (PDOException $e){
+        echo "error : " . $e->getMessage();
+    }
+
+}
+
+//createEleve($db, "Skywalker", "Luc", 18);
+
+$classe = readClasse($db);
 
 
-//createEleve($db, "Solo", "Han", 40);
-readClasse($db);
+//updateEleve($db, 2, "Skywalker", "Luc", 18);
+
+//delEleve($db, 2);
 
